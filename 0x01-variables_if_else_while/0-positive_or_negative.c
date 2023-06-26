8a12d38f335afdc0d4ca6d9f1da3ddfa3a41e45a
elliotsazu <elliotsazu@gmail.com>
2023-06-26 06:48:20 -0700

    positive negative in if else variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
- 
+/*
+ * main- assign a random number to int n everytime
+ * it executes and prints it 
+ * Return: Always 0 (Success)
   */
 int main(void)
 {
