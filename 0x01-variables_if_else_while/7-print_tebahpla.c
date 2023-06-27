8f4f4439b4da57a8dae840bbc4fae67df47b1e6e
elliotsazu <elliotsazu@gmail.com>
2023-06-27 02:56:32 -0700

    print teb

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the lower case in reverse followed by a
+ * new line 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch ='z'; ch >= 'a': ch--)
+	{
+		putchar (ch);
+	}
+		putchar ('\n')
+		return (0);
+}	
