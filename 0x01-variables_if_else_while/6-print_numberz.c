2aead5ae4af702637459ec2343a2b51ce282d439
elliotsazu <elliotsazu@gmail.com>
2023-06-27 02:34:04 -0700

    6-print_umbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - ptints all the single digit numbers 
+ * of base ten starting from zero, followed by 
+ * a new line using puctchar
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
