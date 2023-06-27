c6b05f5bee09eb65f448ecf1cdaa6d69617a9211
elliotsazu <elliotsazu@gmail.com>
2023-06-26 23:45:56 -0700

    merged the two dupliacte swap files

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,9 @@
-#include <stdlib.c>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * main - prints the last digit of random
+ * main - prints the last digit of the random
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -17,9 +17,7 @@ int main(void)
 		printf("and is greater than 5\n");
 	else if (n % 10 == 0)
 		printf("and is 0\n");
-	else if (n % 10 < 6 && n % 10 !=0);
-	printf("and is less than 6 and not \0");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
-		
-	
