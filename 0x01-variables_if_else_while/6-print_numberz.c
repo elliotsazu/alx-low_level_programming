d656bb8d2de6b703f5c25eefb5e889204a83d565
elliotsazu <elliotsazu@gmail.com>
2023-06-27 02:35:04 -0700

    white space removed

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - ptints all the single digit numbers 
- * of base ten starting from zero, followed by 
+ * main - ptints all the single digit numbers
+ * of base ten starting from zero, followed by
  * a new line using puctchar
  * Return: Always 0 (Succes)
  */
