376d9aac92dc41cb1c9e0334f2b815665d95f3e4
elliotsazu <elliotsazu@gmail.com>
2023-06-27 13:22:32 -0700

    check

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,13 +7,14 @@
  */
 
 int main(void)
+
 {
 
 	char ch;
-	for (ch = 'z'; ch >= 'a'; ch--)
+	for(ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
 	}
 		putchar('\n');
-		return (0);
-}	
+		return(0);
+}
