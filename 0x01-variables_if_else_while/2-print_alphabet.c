28e799213a5b95d35b8fad007887c06711a1f9c4
elliotsazu <elliotsazu@gmail.com>
2023-06-27 01:35:15 -0700

    push

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -11,11 +9,12 @@
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar ('\n);
+	putchar ('\n');
 	return (0);
 }
 
