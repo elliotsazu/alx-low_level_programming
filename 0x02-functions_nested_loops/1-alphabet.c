49c4b757c011df0d80cb527ff6764c6842cf11be
elliotsazu <elliotsazu@gmail.com>
2023-09-13 08:10:13 -0700

    print a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * main -a function that prints alphabet
+ *
+ * Return: return 0
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	_putchar ('\n');
+	return (0);
+	}
