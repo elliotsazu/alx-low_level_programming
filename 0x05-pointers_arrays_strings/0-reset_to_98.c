d16bcaf96320f00858b251aafc36fa6ecbeb74f3
elliotsazu <elliotsazu@gmail.com>
2023-09-19 05:35:50 -0700

    removed trailing whotespace

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 -To update the variables value 
- * pointer points to 98 
- * @n: pointer to the variable we update 
+ * reset_to_98 -To update the variables value
+ * pointer points to 98
+ * @n: pointer to the variable we update
  */
 void reset_to_98(int *n)
 {
