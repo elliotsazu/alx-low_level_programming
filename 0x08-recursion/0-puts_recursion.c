e9e6bc32ec06f696568a87e8493b44fbacc5776b
elliotsazu <elliotsazu@gmail.com>
2023-09-28 13:39:11 -0700

    trailing white spaces removed aapted to the betty coding style '

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * _puts_recursion - prints a string, followed by
- * a new line 
- * @s: string to print 
+ * a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
@@ -15,4 +15,3 @@ void _puts_recursion(char *s)
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
-
